361f5591a197bd0c7017866c3b8a21e4df8ad02b
Nate White <natelwhite01@gmail.com>
2026-02-12 00:12:37 -0500

    Module 6 completed baseline

---
+++ app_admin/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TripListing } from './trip-listing/trip-listing';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Travlr Getaways Admin!');
+}
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const tripsController = require('../controllers/trips');
 
 // list all trips
-router.route('/trips').get(tripsController.tripsList);
+router.route('/trips')
+	.get(tripsController.tripsList) // GET Method routes tripList
+	.post(tripsController.tripsAddTrip) // POST Method Adds a Trip
 
 // find trip using parameter
-router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
+router.route('/trips/:tripCode')
+	.get(tripsController.tripsFindByCode)
+	.put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
